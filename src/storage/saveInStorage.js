ceb1880455a09fd419aa8b002b4cf409c5fe7ffd
Djess-V <e.ageevets@yandex.ru>
2023-05-24 20:48:47 +0700

    uninstalled the old application

--- src/storage/saveInStorage.js
+++
@@ -1,7 +0,0 @@
-export async function saveInStorage(items) {
-  try {
-    localStorage.setItem("list", JSON.stringify(items));
-  } catch (e) {
-    console.log("Ошибка сохранения данных в localStorage");
-  }
-}
